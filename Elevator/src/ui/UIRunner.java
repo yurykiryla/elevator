54e841fda8c82f52f9db88abea08757d746b69f7
yurykiryla <yurykiryla@gmail.com>
2014-04-16 16:17:57 +0300

    0.2.3

--- Elevator/src/ui/UIRunner.java
+++ Elevator/src/ui/UIRunner.java
@@ -1,6 +1,5 @@
 package ui;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -9,6 +8,7 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -20,6 +20,8 @@ import javax.swing.JTextArea;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import console.items.Properties;
 
@@ -33,110 +35,132 @@ public class UIRunner {
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jFrame.setResizable(false);
 		
-		JScrollPane jPanelLeft = new JScrollPane();
+		JPanel jPanelLeft = new JPanel();
 		jPanelLeft.setPreferredSize(new Dimension(560, 555));
 		jPanelLeft.setOpaque(true);
 		jPanelLeft.setBorder(BorderFactory.createLineBorder(Color.BLUE));
 		jFrame.add(jPanelLeft);
 		
-		GridBagLayout gridBagLayout = new GridBagLayout();
-		GridBagConstraints gridBagConstraints = new GridBagConstraints();
-		JPanel jPanelRight = new JPanel(gridBagLayout);
+		JPanel jPanelRight = new JPanel(new FlowLayout());
 		jPanelRight.setPreferredSize(new Dimension(220, 555));
 		jPanelRight.setOpaque(true);
 		jPanelRight.setBorder(BorderFactory.createLineBorder(Color.BLUE));
 		
-		JLabel jLabelStoriesNumber = new JLabel("Stories number", SwingConstants.RIGHT);
-		JSpinner jSpinnerStoriesNumber;
-		SpinnerNumberModel snmStoriesNumber = new SpinnerNumberModel(properties.getStoriesNumber(), 2, 9999, 1);
-		jSpinnerStoriesNumber = new JSpinner(snmStoriesNumber);
+		GridBagLayout gridBagLayout = new GridBagLayout();
+		GridBagConstraints gridBagConstraints = new GridBagConstraints();
 		gridBagConstraints.ipadx = 5;
 		gridBagConstraints.weightx = 1.0;
-		gridBagConstraints.insets = new Insets(5, 0, 5, 0);
+		JPanel jPanelParameters = new JPanel(gridBagLayout);
+		jPanelParameters.setBorder(BorderFactory.createEtchedBorder());
+		
+		JLabel jLabelParameters = new JLabel("Parameters");
 		gridBagConstraints.gridx = 0;
 		gridBagConstraints.gridy = 0;
+		gridBagConstraints.anchor = GridBagConstraints.CENTER;
+		gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER;
+		gridBagConstraints.insets = new Insets(5, 0, 10, 0);
+		gridBagLayout.setConstraints(jLabelParameters, gridBagConstraints);
+		jPanelParameters.add(jLabelParameters);
+		
+		JLabel jLabelStoriesNumber = new JLabel("Stories number", SwingConstants.RIGHT);
+		JSpinner jSpinnerStoriesNumber;
+		SpinnerNumberModel spinnerNumberModelStoriesNumber = new SpinnerNumberModel(properties.getStoriesNumber(), 2, 9999, 1);
+		jSpinnerStoriesNumber = new JSpinner(spinnerNumberModelStoriesNumber);
+		gridBagConstraints.insets = new Insets(10, 0, 5, 0);
+		gridBagConstraints.gridx = 0;
+		gridBagConstraints.gridy = 1;
 		gridBagConstraints.anchor = GridBagConstraints.WEST;
 		gridBagLayout.setConstraints(jLabelStoriesNumber, gridBagConstraints);
 		gridBagConstraints.gridx = 1;
-		gridBagConstraints.gridy = 0;
+		gridBagConstraints.gridy = 1;
 		gridBagConstraints.anchor = GridBagConstraints.EAST;
 		gridBagLayout.setConstraints(jSpinnerStoriesNumber, gridBagConstraints);
-		jPanelRight.add(jLabelStoriesNumber);
-		jPanelRight.add(jSpinnerStoriesNumber);
-		
+		jPanelParameters.add(jLabelStoriesNumber);
+		jPanelParameters.add(jSpinnerStoriesNumber);
 		
 		JLabel jLabelElevatorCapacity = new JLabel("Elevator capacity", SwingConstants.RIGHT);
-		SpinnerNumberModel snmElevatorCapacity = new SpinnerNumberModel(properties.getElevatorCapacity(), 1, 9999, 1);
-		JSpinner jSpinnerElevatorCapacity = new JSpinner(snmElevatorCapacity);
+		SpinnerNumberModel spinnerNumberModelElevatorCapacity = new SpinnerNumberModel(properties.getElevatorCapacity(), 1, 9999, 1);
+		JSpinner jSpinnerElevatorCapacity = new JSpinner(spinnerNumberModelElevatorCapacity);
 		gridBagConstraints.gridx = 0;
-		gridBagConstraints.gridy = 1;
+		gridBagConstraints.gridy = 2;
 		gridBagConstraints.anchor = GridBagConstraints.WEST;
 		gridBagLayout.setConstraints(jLabelElevatorCapacity, gridBagConstraints);
 		gridBagConstraints.gridx = 1;
-		gridBagConstraints.gridy = 1;
+		gridBagConstraints.gridy = 2;
 		gridBagConstraints.anchor = GridBagConstraints.EAST;
 		gridBagLayout.setConstraints(jSpinnerElevatorCapacity, gridBagConstraints);
-		jPanelRight.add(jLabelElevatorCapacity);
-		jPanelRight.add(jSpinnerElevatorCapacity);
+		jPanelParameters.add(jLabelElevatorCapacity);
+		jPanelParameters.add(jSpinnerElevatorCapacity);
 		
 		JLabel jLabelPassengersNumber = new JLabel("Passengers number", SwingConstants.RIGHT);
 		SpinnerNumberModel spinnerNumberModelPassengersNumber = new SpinnerNumberModel(properties.getPassengersNumber(), 1, 9999, 1);
 		JSpinner jSpinnerPassengersNumber = new JSpinner(spinnerNumberModelPassengersNumber);
 		gridBagConstraints.gridx = 0;
-		gridBagConstraints.gridy = 2;
+		gridBagConstraints.gridy = 3;
 		gridBagConstraints.anchor = GridBagConstraints.WEST;
 		gridBagLayout.setConstraints(jLabelPassengersNumber, gridBagConstraints);
 		gridBagConstraints.gridx = 1;
-		gridBagConstraints.gridy = 2;
+		gridBagConstraints.gridy = 3;
 		gridBagConstraints.anchor = GridBagConstraints.EAST;
 		gridBagLayout.setConstraints(jSpinnerPassengersNumber, gridBagConstraints);
-		jPanelRight.add(jLabelPassengersNumber);
-		jPanelRight.add(jSpinnerPassengersNumber);
+		jPanelParameters.add(jLabelPassengersNumber);
+		jPanelParameters.add(jSpinnerPassengersNumber);
 		
-		JSlider jSliderAnimationBoost = new JSlider(0, 100, properties.getAnimationBoost());
+		final JSlider jSliderAnimationBoost = new JSlider(0, 100, properties.getAnimationBoost());
 		jSliderAnimationBoost.setMajorTickSpacing(20);
 		jSliderAnimationBoost.setMinorTickSpacing(5);
 		jSliderAnimationBoost.setLabelTable(jSliderAnimationBoost.createStandardLabels(20));
 		jSliderAnimationBoost.setPaintTicks(true);
 		jSliderAnimationBoost.setPaintLabels(true);
-		JLabel jLabelAnimationBoost = new JLabel("Animation Boost");
-		gridBagConstraints.gridx = 0;
-		gridBagConstraints.gridy = 3;
-		gridBagConstraints.anchor = GridBagConstraints.CENTER;
-		gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER;
-		gridBagConstraints.insets = new Insets(5, 0, 0, 0);
-		gridBagLayout.setConstraints(jSliderAnimationBoost, gridBagConstraints);
+		final JLabel jLabelAnimationBoost = new JLabel("Animation Boost (" + jSliderAnimationBoost.getValue() + ")");
+		jSliderAnimationBoost.addChangeListener(new ChangeListener() {
+			@Override
+			public void stateChanged(ChangeEvent e) {
+				// TODO Auto-generated method stub
+				jLabelAnimationBoost.setText("Animation Boost (" + jSliderAnimationBoost.getValue() + ")");
+			}
+		});
 		gridBagConstraints.gridx = 0;
 		gridBagConstraints.gridy = 4;
-		gridBagConstraints.insets = new Insets(0, 0, 5, 0);
-		gridBagLayout.setConstraints(jLabelAnimationBoost, gridBagConstraints);
-		jPanelRight.add(jSliderAnimationBoost);
-		jPanelRight.add(jLabelAnimationBoost);
-		
-		JButton jButton = new JButton("START");
+		gridBagConstraints.anchor = GridBagConstraints.CENTER;
+		gridBagConstraints.gridwidth = GridBagConstraints.REMAINDER;
+		gridBagConstraints.insets = new Insets(15, 0, 0, 0);
+		gridBagLayout.setConstraints(jSliderAnimationBoost, gridBagConstraints);
 		gridBagConstraints.gridx = 0;
 		gridBagConstraints.gridy = 5;
-		gridBagConstraints.insets = new Insets(10, 0, 5, 0);
-		gridBagLayout.setConstraints(jButton, gridBagConstraints);
-		jPanelRight.add(jButton);
+		gridBagConstraints.insets = new Insets(0, 0, 15, 0);
+		gridBagLayout.setConstraints(jLabelAnimationBoost, gridBagConstraints);
+		jPanelParameters.add(jSliderAnimationBoost);
+		jPanelParameters.add(jLabelAnimationBoost);
 		
+		Box box = Box.createVerticalBox();
+		box.add(jPanelParameters);
+		
+		box.add(Box.createRigidArea(new Dimension(100, 30)));
+		
+		JButton jButton = new JButton("START");
+		JPanel jPanelButton = new JPanel();
+		jPanelButton.add(jButton);
+		box.add(jPanelButton);
+	
+		box.add(Box.createRigidArea(new Dimension(100, 30)));
 		
 		JTextArea jTextArea = new JTextArea("Please select parameters and Start");
-		jTextArea.setBorder(BorderFactory.createLineBorder(Color.BLACK));
-		jTextArea.setVisible(true);
+		jTextArea.setLineWrap(true);
+		jTextArea.setWrapStyleWord(true);
+		jTextArea.setEditable(false);
 		JScrollPane jScrollPaneText = new JScrollPane(jTextArea);
+		jScrollPaneText.setPreferredSize(new Dimension(200, 220));
+		box.add(jScrollPaneText);
 		
-		gridBagConstraints.gridx = 0;
-		gridBagConstraints.gridy = 6;
-		gridBagConstraints.insets = new Insets(10, 0, 5, 0);
-		gridBagLayout.setConstraints(jScrollPaneText, gridBagConstraints);
-		jPanelRight.add(jScrollPaneText);
-		
+		jPanelRight.add(box);
 		
 		jFrame.add(jPanelRight);
 		
 		jFrame.setVisible(true);
 	}
+	
+	
 
 	public static void start(Properties properties){
 		UIRunner.properties = properties;
