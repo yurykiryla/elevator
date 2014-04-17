de3263c4b8da75b01b2f3f737e74e6d738f2dd63
yurykiryla <yurykiryla@gmail.com>
2014-04-17 14:07:15 +0300

    0.2.4

--- Elevator/src/ui/UIRunner.java
+++ Elevator/src/ui/UIRunner.java
@@ -6,6 +6,8 @@ import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.Box;
@@ -23,13 +25,27 @@ import javax.swing.SwingUtilities;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
+import console.items.Building;
 import console.items.Properties;
+import console.transportation.Controller;
 
-public class UIRunner {
+public class UIRunner implements ChangeListener, ActionListener{
 	private static Properties properties;
+	private Building building;
+	private static final String BUTTON_START = "START";
+	private static final String BUTTON_ABORT = "ABORT";
+	private static final String BUTTON_VIEW_LOG = "VIEW LOG";
+	private JFrame jFrame;
+	private JSlider jSliderAnimationBoost;
+	private JLabel jLabelAnimationBoost;
+	private JButton jButton;
+	private JSpinner jSpinnerStoriesNumber;
+	private JSpinner jSpinnerElevatorCapacity;
+	private JSpinner jSpinnerPassengersNumber;
+	
 	
 	public UIRunner() {
-		JFrame jFrame = new JFrame("Elevator");
+		jFrame = new JFrame("Elevator");
 		jFrame.setLayout(new FlowLayout());
 		jFrame.setSize(800, 600);
 		jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -63,7 +79,6 @@ public class UIRunner {
 		jPanelParameters.add(jLabelParameters);
 		
 		JLabel jLabelStoriesNumber = new JLabel("Stories number", SwingConstants.RIGHT);
-		JSpinner jSpinnerStoriesNumber;
 		SpinnerNumberModel spinnerNumberModelStoriesNumber = new SpinnerNumberModel(properties.getStoriesNumber(), 2, 9999, 1);
 		jSpinnerStoriesNumber = new JSpinner(spinnerNumberModelStoriesNumber);
 		gridBagConstraints.insets = new Insets(10, 0, 5, 0);
@@ -80,7 +95,7 @@ public class UIRunner {
 		
 		JLabel jLabelElevatorCapacity = new JLabel("Elevator capacity", SwingConstants.RIGHT);
 		SpinnerNumberModel spinnerNumberModelElevatorCapacity = new SpinnerNumberModel(properties.getElevatorCapacity(), 1, 9999, 1);
-		JSpinner jSpinnerElevatorCapacity = new JSpinner(spinnerNumberModelElevatorCapacity);
+		jSpinnerElevatorCapacity = new JSpinner(spinnerNumberModelElevatorCapacity);
 		gridBagConstraints.gridx = 0;
 		gridBagConstraints.gridy = 2;
 		gridBagConstraints.anchor = GridBagConstraints.WEST;
@@ -94,7 +109,7 @@ public class UIRunner {
 		
 		JLabel jLabelPassengersNumber = new JLabel("Passengers number", SwingConstants.RIGHT);
 		SpinnerNumberModel spinnerNumberModelPassengersNumber = new SpinnerNumberModel(properties.getPassengersNumber(), 1, 9999, 1);
-		JSpinner jSpinnerPassengersNumber = new JSpinner(spinnerNumberModelPassengersNumber);
+		jSpinnerPassengersNumber = new JSpinner(spinnerNumberModelPassengersNumber);
 		gridBagConstraints.gridx = 0;
 		gridBagConstraints.gridy = 3;
 		gridBagConstraints.anchor = GridBagConstraints.WEST;
@@ -106,20 +121,14 @@ public class UIRunner {
 		jPanelParameters.add(jLabelPassengersNumber);
 		jPanelParameters.add(jSpinnerPassengersNumber);
 		
-		final JSlider jSliderAnimationBoost = new JSlider(0, 100, properties.getAnimationBoost());
+		jSliderAnimationBoost = new JSlider(0, 100, properties.getAnimationBoost());
 		jSliderAnimationBoost.setMajorTickSpacing(20);
 		jSliderAnimationBoost.setMinorTickSpacing(5);
 		jSliderAnimationBoost.setLabelTable(jSliderAnimationBoost.createStandardLabels(20));
 		jSliderAnimationBoost.setPaintTicks(true);
 		jSliderAnimationBoost.setPaintLabels(true);
-		final JLabel jLabelAnimationBoost = new JLabel("Animation Boost (" + jSliderAnimationBoost.getValue() + ")");
-		jSliderAnimationBoost.addChangeListener(new ChangeListener() {
-			@Override
-			public void stateChanged(ChangeEvent e) {
-				// TODO Auto-generated method stub
-				jLabelAnimationBoost.setText("Animation Boost (" + jSliderAnimationBoost.getValue() + ")");
-			}
-		});
+		jSliderAnimationBoost.addChangeListener(this);
+		jLabelAnimationBoost = new JLabel("Animation Boost (" + jSliderAnimationBoost.getValue() + ")");
 		gridBagConstraints.gridx = 0;
 		gridBagConstraints.gridy = 4;
 		gridBagConstraints.anchor = GridBagConstraints.CENTER;
@@ -138,7 +147,8 @@ public class UIRunner {
 		
 		box.add(Box.createRigidArea(new Dimension(100, 30)));
 		
-		JButton jButton = new JButton("START");
+		jButton = new JButton(BUTTON_START);
+		jButton.addActionListener(this);
 		JPanel jPanelButton = new JPanel();
 		jPanelButton.add(jButton);
 		box.add(jPanelButton);
@@ -159,8 +169,46 @@ public class UIRunner {
 		
 		jFrame.setVisible(true);
 	}
-	
-	
+
+	@Override
+	public void stateChanged(ChangeEvent e) {
+		// TODO Auto-generated method stub
+		jLabelAnimationBoost.setText("Animation Boost (" + jSliderAnimationBoost.getValue() + ")");
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		switch (e.getActionCommand()){
+			case BUTTON_START:
+				int storiesNumber = Integer.parseInt(jSpinnerStoriesNumber.getValue().toString());
+				int elevatorCapacity = Integer.parseInt(jSpinnerElevatorCapacity.getValue().toString());
+				int passengersNumber = Integer.parseInt(jSpinnerPassengersNumber.getValue().toString());
+				int animationBoost = jSliderAnimationBoost.getValue();
+				properties = new Properties(storiesNumber, elevatorCapacity, passengersNumber, animationBoost);
+				building = new Building(properties);
+				if(animationBoost == 0){
+					Controller controller = building.getController();
+					controller.run();
+					jFrame.setVisible(false);
+					System.exit(0);
+				}else{
+					jSpinnerStoriesNumber.setEnabled(false);
+					jSpinnerElevatorCapacity.setEnabled(false);
+					jSpinnerPassengersNumber.setEnabled(false);
+					jSliderAnimationBoost.setEnabled(false);
+					jLabelAnimationBoost.setEnabled(false);
+					jButton.setText(BUTTON_ABORT);
+				}
+				break;
+			case BUTTON_ABORT:
+				building.getThreadGroup().interrupt();
+				jButton.setText(BUTTON_VIEW_LOG);
+				break;
+			case BUTTON_VIEW_LOG:
+				break;
+		}
+	}
 
 	public static void start(Properties properties){
 		UIRunner.properties = properties;
